1afd1f1ea05f6e543fa4665e680273ef7c69598d
Roman Filatov <ikardoc@gmail.com>
2022-02-23 16:24:05 +0300

    Подключил PostgreSQL Scaffold-DbContext

---
+++ Models/PerevalAdded.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FstrApi.Models
+{
+    public partial class PerevalAdded
+    {
+        public int Id { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public string? RawData { get; set; }
+        public string? Images { get; set; }
+    }
+}
---
+++ Models/SprActivitiesType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FstrApi.Models
+{
+    public partial class SprActivitiesType
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+    }
+}
