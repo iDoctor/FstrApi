c685a05e47e15d932382bfecea05cd5d577d2c71
Roman Filatov <ikardoc@gmail.com>
2022-02-25 11:53:18 +0300

    Добавил настройку строки подключения через Системные Переменные

---
+++ Connection.cs
@@ -0,0 +1,24 @@
+﻿namespace FstrApi
+{
+    public static class Connection
+    {
+        public static string GetConnectionString()
+        {
+            string? host = Environment.GetEnvironmentVariable("FSTR_DB_HOST");
+            string? port = Environment.GetEnvironmentVariable("FSTR_DB_PORT");
+            string? login = Environment.GetEnvironmentVariable("FSTR_DB_LOGIN");
+            string? password = Environment.GetEnvironmentVariable("FSTR_DB_PASS");
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("FSTR_DB_HOST", EnvironmentVariableTarget.Machine)))
+            {
+                host = Environment.GetEnvironmentVariable("FSTR_DB_HOST", EnvironmentVariableTarget.Machine);
+                port = Environment.GetEnvironmentVariable("FSTR_DB_PORT", EnvironmentVariableTarget.Machine);
+                login = Environment.GetEnvironmentVariable("FSTR_DB_LOGIN", EnvironmentVariableTarget.Machine);
+                password = Environment.GetEnvironmentVariable("FSTR_DB_PASS", EnvironmentVariableTarget.Machine);
+            }
+
+
+            return $"Host={host};Port={port};Database=FSTR_DB;Username={login};Password={password}";
+        }
+    }
+}
